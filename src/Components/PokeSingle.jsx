f4c48b21739768d6accc56642fd13ac7dde30ffe
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2022-11-29 18:57:08 +0200

    pokesingle

--- src/Components/PokeSingle.jsx
+++ src/Components/PokeSingle.jsx
@@ -16,6 +16,7 @@ const PokeSingle = () => {
         <div>
             <h2>{data.name}</h2>
             <img src={data.sprites?.other.home.front_default} alt={data.name} />
+            <div>{data.types.type}</div>
         </div>
     );
 };
